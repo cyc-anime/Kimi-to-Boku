42468185f3d8b6824c16f2a2c3f6e890f11e66d0
Cryptic <crypticvideos@gmail.com>
2019-02-19 18:57:54 -0500

    v2 Patch Files

---
+++ !v2 Patches/3.Apply Patch-Linux.sh
@@ -0,0 +1,29 @@
+` #!/bin/sh`
+` mkdir old`
+` chmod +x xdelta3`
+` xdelta3 -v -d -s "[CyC] Kimi to Boku 01 (BD 1080p Hi10 x264 FLAC) [C400580C].mkv" "vcdiff/[CyC] Kimi to Boku 01 (BD 1080p Hi10 x264 FLAC) [C400580C].mkv.vcdiff" "[CyC] Kimi to Boku 01v2 (BD 1080p Hi10 x264 FLAC) [9F0E17B7].mkv"`
+` mv "[CyC] Kimi to Boku 01 (BD 1080p Hi10 x264 FLAC) [C400580C].mkv" old`
+` xdelta3 -v -d -s "[CyC] Kimi to Boku 02 (BD 1080p Hi10 x264 FLAC) [2D081845].mkv" "vcdiff/[CyC] Kimi to Boku 02 (BD 1080p Hi10 x264 FLAC) [2D081845].mkv.vcdiff" "[CyC] Kimi to Boku 02v2 (BD 1080p Hi10 x264 FLAC) [375E748D].mkv"`
+` mv "[CyC] Kimi to Boku 02 (BD 1080p Hi10 x264 FLAC) [2D081845].mkv" old`
+` xdelta3 -v -d -s "[CyC] Kimi to Boku 03 (BD 1080p Hi10 x264 FLAC) [9748D6F8].mkv" "vcdiff/[CyC] Kimi to Boku 03 (BD 1080p Hi10 x264 FLAC) [9748D6F8].mkv.vcdiff" "[CyC] Kimi to Boku 03v2 (BD 1080p Hi10 x264 FLAC) [49680801].mkv"`
+` mv "[CyC] Kimi to Boku 03 (BD 1080p Hi10 x264 FLAC) [9748D6F8].mkv" old`
+` xdelta3 -v -d -s "[CyC] Kimi to Boku 04 (BD 1080p Hi10 x264 FLAC) [8CE6ADB5].mkv" "vcdiff/[CyC] Kimi to Boku 04 (BD 1080p Hi10 x264 FLAC) [8CE6ADB5].mkv.vcdiff" "[CyC] Kimi to Boku 04v2 (BD 1080p Hi10 x264 FLAC) [D2D5ED72].mkv"`
+` mv "[CyC] Kimi to Boku 04 (BD 1080p Hi10 x264 FLAC) [8CE6ADB5].mkv" old`
+` xdelta3 -v -d -s "[CyC] Kimi to Boku 05 (BD 1080p Hi10 x264 FLAC) [668F1537].mkv" "vcdiff/[CyC] Kimi to Boku 05 (BD 1080p Hi10 x264 FLAC) [668F1537].mkv.vcdiff" "[CyC] Kimi to Boku 05v2 (BD 1080p Hi10 x264 FLAC) [BFCFA212].mkv"`
+` mv "[CyC] Kimi to Boku 05 (BD 1080p Hi10 x264 FLAC) [668F1537].mkv" old`
+` xdelta3 -v -d -s "[CyC] Kimi to Boku 06 (BD 1080p Hi10 x264 FLAC) [4C839133].mkv" "vcdiff/[CyC] Kimi to Boku 06 (BD 1080p Hi10 x264 FLAC) [4C839133].mkv.vcdiff" "[CyC] Kimi to Boku 06v2 (BD 1080p Hi10 x264 FLAC) [E065DF01].mkv"`
+` mv "[CyC] Kimi to Boku 06 (BD 1080p Hi10 x264 FLAC) [4C839133].mkv" old`
+` xdelta3 -v -d -s "[CyC] Kimi to Boku 07 (BD 1080p Hi10 x264 FLAC) [1858EC1F].mkv" "vcdiff/[CyC] Kimi to Boku 07 (BD 1080p Hi10 x264 FLAC) [1858EC1F].mkv.vcdiff" "[CyC] Kimi to Boku 07v2 (BD 1080p Hi10 x264 FLAC) [17B7CA7C].mkv"`
+` mv "[CyC] Kimi to Boku 07 (BD 1080p Hi10 x264 FLAC) [1858EC1F].mkv" old`
+` xdelta3 -v -d -s "[CyC] Kimi to Boku 08 (BD 1080p Hi10 x264 FLAC) [AC3F72A0].mkv" "vcdiff/[CyC] Kimi to Boku 08 (BD 1080p Hi10 x264 FLAC) [AC3F72A0].mkv.vcdiff" "[CyC] Kimi to Boku 08v2 (BD 1080p Hi10 x264 FLAC) [327FD0C7].mkv"`
+` mv "[CyC] Kimi to Boku 08 (BD 1080p Hi10 x264 FLAC) [AC3F72A0].mkv" old`
+` xdelta3 -v -d -s "[CyC] Kimi to Boku 09 (BD 1080p Hi10 x264 FLAC) [6A1D1761].mkv" "vcdiff/[CyC] Kimi to Boku 09 (BD 1080p Hi10 x264 FLAC) [6A1D1761].mkv.vcdiff" "[CyC] Kimi to Boku 09v2 (BD 1080p Hi10 x264 FLAC) [8F95285A].mkv"`
+` mv "[CyC] Kimi to Boku 09 (BD 1080p Hi10 x264 FLAC) [6A1D1761].mkv" old`
+` xdelta3 -v -d -s "[CyC] Kimi to Boku 10 (BD 1080p Hi10 x264 FLAC) [8EA38565].mkv" "vcdiff/[CyC] Kimi to Boku 10 (BD 1080p Hi10 x264 FLAC) [8EA38565].mkv.vcdiff" "[CyC] Kimi to Boku 10v2 (BD 1080p Hi10 x264 FLAC) [943D3F34].mkv"`
+` mv "[CyC] Kimi to Boku 10 (BD 1080p Hi10 x264 FLAC) [8EA38565].mkv" old`
+` xdelta3 -v -d -s "[CyC] Kimi to Boku 11 (BD 1080p Hi10 x264 FLAC) [BCED228A].mkv" "vcdiff/[CyC] Kimi to Boku 11 (BD 1080p Hi10 x264 FLAC) [BCED228A].mkv.vcdiff" "[CyC] Kimi to Boku 11v2 (BD 1080p Hi10 x264 FLAC) [A08F4DFC].mkv"`
+` mv "[CyC] Kimi to Boku 11 (BD 1080p Hi10 x264 FLAC) [BCED228A].mkv" old`
+` xdelta3 -v -d -s "[CyC] Kimi to Boku 12 (BD 1080p Hi10 x264 FLAC) [A2C29148].mkv" "vcdiff/[CyC] Kimi to Boku 12 (BD 1080p Hi10 x264 FLAC) [A2C29148].mkv.vcdiff" "[CyC] Kimi to Boku 12v2 (BD 1080p Hi10 x264 FLAC) [1901C15A].mkv"`
+` mv "[CyC] Kimi to Boku 12 (BD 1080p Hi10 x264 FLAC) [A2C29148].mkv" old`
+` xdelta3 -v -d -s "[CyC] Kimi to Boku 13 (BD 1080p Hi10 x264 FLAC) [B7E9B583].mkv" "vcdiff/[CyC] Kimi to Boku 13 (BD 1080p Hi10 x264 FLAC) [B7E9B583].mkv.vcdiff" "[CyC] Kimi to Boku 13v2 (BD 1080p Hi10 x264 FLAC) [5728904D].mkv"`
+` mv "[CyC] Kimi to Boku 13 (BD 1080p Hi10 x264 FLAC) [B7E9B583].mkv" old`
---
+++ !v2 Patches/3.Apply Patch-Windows.bat
@@ -0,0 +1,30 @@
+@echo off
+mkdir old
+.\xdelta-3.1.0-x86_64.exe -v -d -s "[CyC] Kimi to Boku 01 (BD 1080p Hi10 x264 FLAC) [C400580C].mkv" ".\vcdiff\[CyC] Kimi to Boku 01 (BD 1080p Hi10 x264 FLAC) [C400580C].mkv.vcdiff" "[CyC] Kimi to Boku 01v2 (BD 1080p Hi10 x264 FLAC) [9F0E17B7].mkv"
+move "[CyC] Kimi to Boku 01 (BD 1080p Hi10 x264 FLAC) [C400580C].mkv" old
+.\xdelta-3.1.0-x86_64.exe -v -d -s "[CyC] Kimi to Boku 02 (BD 1080p Hi10 x264 FLAC) [2D081845].mkv" ".\vcdiff\[CyC] Kimi to Boku 02 (BD 1080p Hi10 x264 FLAC) [2D081845].mkv.vcdiff" "[CyC] Kimi to Boku 02v2 (BD 1080p Hi10 x264 FLAC) [375E748D].mkv"
+move "[CyC] Kimi to Boku 02 (BD 1080p Hi10 x264 FLAC) [2D081845].mkv" old
+.\xdelta-3.1.0-x86_64.exe -v -d -s "[CyC] Kimi to Boku 03 (BD 1080p Hi10 x264 FLAC) [9748D6F8].mkv" ".\vcdiff\[CyC] Kimi to Boku 03 (BD 1080p Hi10 x264 FLAC) [9748D6F8].mkv.vcdiff" "[CyC] Kimi to Boku 03v2 (BD 1080p Hi10 x264 FLAC) [49680801].mkv"
+move "[CyC] Kimi to Boku 03 (BD 1080p Hi10 x264 FLAC) [9748D6F8].mkv" old
+.\xdelta-3.1.0-x86_64.exe -v -d -s "[CyC] Kimi to Boku 04 (BD 1080p Hi10 x264 FLAC) [8CE6ADB5].mkv" ".\vcdiff\[CyC] Kimi to Boku 04 (BD 1080p Hi10 x264 FLAC) [8CE6ADB5].mkv.vcdiff" "[CyC] Kimi to Boku 04v2 (BD 1080p Hi10 x264 FLAC) [D2D5ED72].mkv"
+move "[CyC] Kimi to Boku 04 (BD 1080p Hi10 x264 FLAC) [8CE6ADB5].mkv" old
+.\xdelta-3.1.0-x86_64.exe -v -d -s "[CyC] Kimi to Boku 05 (BD 1080p Hi10 x264 FLAC) [668F1537].mkv" ".\vcdiff\[CyC] Kimi to Boku 05 (BD 1080p Hi10 x264 FLAC) [668F1537].mkv.vcdiff" "[CyC] Kimi to Boku 05v2 (BD 1080p Hi10 x264 FLAC) [BFCFA212].mkv"
+move "[CyC] Kimi to Boku 05 (BD 1080p Hi10 x264 FLAC) [668F1537].mkv" old
+.\xdelta-3.1.0-x86_64.exe -v -d -s "[CyC] Kimi to Boku 06 (BD 1080p Hi10 x264 FLAC) [4C839133].mkv" ".\vcdiff\[CyC] Kimi to Boku 06 (BD 1080p Hi10 x264 FLAC) [4C839133].mkv.vcdiff" "[CyC] Kimi to Boku 06v2 (BD 1080p Hi10 x264 FLAC) [E065DF01].mkv"
+move "[CyC] Kimi to Boku 06 (BD 1080p Hi10 x264 FLAC) [4C839133].mkv" old
+.\xdelta-3.1.0-x86_64.exe -v -d -s "[CyC] Kimi to Boku 07 (BD 1080p Hi10 x264 FLAC) [1858EC1F].mkv" ".\vcdiff\[CyC] Kimi to Boku 07 (BD 1080p Hi10 x264 FLAC) [1858EC1F].mkv.vcdiff" "[CyC] Kimi to Boku 07v2 (BD 1080p Hi10 x264 FLAC) [17B7CA7C].mkv"
+move "[CyC] Kimi to Boku 07 (BD 1080p Hi10 x264 FLAC) [1858EC1F].mkv" old
+.\xdelta-3.1.0-x86_64.exe -v -d -s "[CyC] Kimi to Boku 08 (BD 1080p Hi10 x264 FLAC) [AC3F72A0].mkv" ".\vcdiff\[CyC] Kimi to Boku 08 (BD 1080p Hi10 x264 FLAC) [AC3F72A0].mkv.vcdiff" "[CyC] Kimi to Boku 08v2 (BD 1080p Hi10 x264 FLAC) [327FD0C7].mkv"
+move "[CyC] Kimi to Boku 08 (BD 1080p Hi10 x264 FLAC) [AC3F72A0].mkv" old
+.\xdelta-3.1.0-x86_64.exe -v -d -s "[CyC] Kimi to Boku 09 (BD 1080p Hi10 x264 FLAC) [6A1D1761].mkv" ".\vcdiff\[CyC] Kimi to Boku 09 (BD 1080p Hi10 x264 FLAC) [6A1D1761].mkv.vcdiff" "[CyC] Kimi to Boku 09v2 (BD 1080p Hi10 x264 FLAC) [8F95285A].mkv"
+move "[CyC] Kimi to Boku 09 (BD 1080p Hi10 x264 FLAC) [6A1D1761].mkv" old
+.\xdelta-3.1.0-x86_64.exe -v -d -s "[CyC] Kimi to Boku 10 (BD 1080p Hi10 x264 FLAC) [8EA38565].mkv" ".\vcdiff\[CyC] Kimi to Boku 10 (BD 1080p Hi10 x264 FLAC) [8EA38565].mkv.vcdiff" "[CyC] Kimi to Boku 10v2 (BD 1080p Hi10 x264 FLAC) [943D3F34].mkv"
+move "[CyC] Kimi to Boku 10 (BD 1080p Hi10 x264 FLAC) [8EA38565].mkv" old
+.\xdelta-3.1.0-x86_64.exe -v -d -s "[CyC] Kimi to Boku 11 (BD 1080p Hi10 x264 FLAC) [BCED228A].mkv" ".\vcdiff\[CyC] Kimi to Boku 11 (BD 1080p Hi10 x264 FLAC) [BCED228A].mkv.vcdiff" "[CyC] Kimi to Boku 11v2 (BD 1080p Hi10 x264 FLAC) [A08F4DFC].mkv"
+move "[CyC] Kimi to Boku 11 (BD 1080p Hi10 x264 FLAC) [BCED228A].mkv" old
+.\xdelta-3.1.0-x86_64.exe -v -d -s "[CyC] Kimi to Boku 12 (BD 1080p Hi10 x264 FLAC) [A2C29148].mkv" ".\vcdiff\[CyC] Kimi to Boku 12 (BD 1080p Hi10 x264 FLAC) [A2C29148].mkv.vcdiff" "[CyC] Kimi to Boku 12v2 (BD 1080p Hi10 x264 FLAC) [1901C15A].mkv"
+move "[CyC] Kimi to Boku 12 (BD 1080p Hi10 x264 FLAC) [A2C29148].mkv" old
+.\xdelta-3.1.0-x86_64.exe -v -d -s "[CyC] Kimi to Boku 13 (BD 1080p Hi10 x264 FLAC) [B7E9B583].mkv" ".\vcdiff\[CyC] Kimi to Boku 13 (BD 1080p Hi10 x264 FLAC) [B7E9B583].mkv.vcdiff" "[CyC] Kimi to Boku 13v2 (BD 1080p Hi10 x264 FLAC) [5728904D].mkv"
+move "[CyC] Kimi to Boku 13 (BD 1080p Hi10 x264 FLAC) [B7E9B583].mkv" old
+echo Completed!
+@pause
